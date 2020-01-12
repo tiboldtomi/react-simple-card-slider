56e43f8d9893705084a70e5afd3da2178bf9faa5
Tamás Tibold <tiboldtomi94@gmail.com>
2020-01-12 20:11:58 +0100

    Updates

---
+++ precache-manifest.02f177401673aa349eca32ed18b07424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5707b01eebb9c2557a75e0345e02ee5",
+    "url": "/react-simple-card-slider/index.html"
+  },
+  {
+    "revision": "e0453281a1934ddfaf4a",
+    "url": "/react-simple-card-slider/static/css/main.2887204c.chunk.css"
+  },
+  {
+    "revision": "d58f77912110f0df1ccb",
+    "url": "/react-simple-card-slider/static/js/2.9c7f1829.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-simple-card-slider/static/js/2.9c7f1829.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0453281a1934ddfaf4a",
+    "url": "/react-simple-card-slider/static/js/main.9b052bad.chunk.js"
+  },
+  {
+    "revision": "4aea8269b5cda7fc53fe",
+    "url": "/react-simple-card-slider/static/js/runtime-main.fe556e7a.js"
+  }
+]);
